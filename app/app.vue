14da355640d15bacdd07ecf7d3a6e735a385fa0a
DesiredName <wc3-enter@yandex.ru>
2025-09-02 09:39:26 +0200

    update

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    hi
+  <div class="bg-sig-yellow">
+    hi {{ result }}
   </div>
 </template>
+
+<script setup lang="ts">
+const result = ref<string>('waiting...')
+
+onMounted(async () => {
+  result.value = await $fetch<string>('/api/test');
+})
+</script>
