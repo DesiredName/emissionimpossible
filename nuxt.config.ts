3db818f2409db7c4db99a60105fe0691f5ed1b86
DesiredName <wc3-enter@yandex.ru>
2025-09-02 09:31:57 +0200

    update

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    display: 'block',
+    families: {
+      Poppins: true,
+    }
+  }
+})
